5399875491ef7cb6202e1f7300b79a3d1e54b2bc
Liz Leaverton <Liz Leaverton>
2017-07-24 13:57:23 -0500

    added pause capabilities

--- javascript/gifpull.js
+++ javascript/gifpull.js
@@ -1,4 +1,5 @@
 var feelings = ["happy", "sad", "scared", "excited"];
+var urlstring;
 
 function renderButtons(){
 	$("#buttons-appear-here").empty();
@@ -15,6 +16,7 @@ function renderButtons(){
 
 function showGifs(){
 	$("#gifs-appear-here").empty();
+	$(this).addClass("active");
 	console.log($(this).attr("class"));
 	var clickFeeling = $(this).attr("data-emotion");
 	console.log(clickFeeling);
@@ -33,13 +35,36 @@ function showGifs(){
             var p = $("<p>");
 	        p.text("Rating: "+results[i].rating);
 	        var emotionImage = $("<img>");
-	        emotionImage.attr("src",results[i].images.fixed_height.url);
+	        urlstring = (results[i].images.fixed_height.url).toString();
+	        console.log(urlstring);
+	        stillurl = urlstring.replace('200','200_s');
+	        console.log(stillurl)
+	        emotionImage.attr("src",stillurl);
+	        emotionImage.attr("data-state","still");
+	        emotionImage.addClass("gif");
 	        emotionDiv.append(p);
 	        emotionDiv.append(emotionImage);
-	        $("#gifs-appear-here").last().after(emotionDiv);
+	        $("#gifs-appear-here").prepend(emotionDiv);
 	    }
 	});
 
+};
+
+function animateGif(){
+	console.log("click works");
+	var state = $(this).attr("data-state");
+	if (state === "still"){
+        stillurl = $(this).attr("src");
+		animateurl = stillurl.replace('200_s','200');
+		$(this).attr("src",animateurl);
+        $(this).attr("data-state","animate");
+      }
+    else {
+        animateurl = $(this).attr("src");
+        stillurl = animateurl.replace('200','200_s');
+        $(this).attr("src",stillurl);
+        $(this).attr("data-state","still");
+      }
 };
 
 $("#find-emotion").on("click", function(event) {
@@ -51,6 +76,8 @@ $("#find-emotion").on("click", function(event) {
 });
 
 $(document).on("click", ".feeling", showGifs);
+
+$(document).on("click", ".gif", animateGif);
 
 renderButtons();
 
